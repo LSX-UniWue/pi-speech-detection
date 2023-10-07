1c62c33d42d80ae5febeb83ff62b899bccb0a7a9
phillis94 <101096552+phillis94@users.noreply.github.com>
2023-10-07 14:31:30 +0200

    Create sound.sh

---
+++ services_examples/sound.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+SAMPLE_RATE=44100
+DESTINATION="/tmp/we4bee/audio"  # location in the temporary filesystem, where the soundfiles are to be saved to
+LOCATION="${HOME}/code-we4bee-sensor_network/scripts/sound_compression.sh"  # location of the sound compression script
+DEVICE="plughw:Device"  # ID of the used microphone
+CHUNK_SIZE_IN_S=60  # 1 minute
+QUALITY="cd"
+CHANNELS=2
+
+while :
+do
+    ts=$( date +%Y-%m-%dT%H:%M:%S%z )
+    file="${DESTINATION}/${ts}"
+    arecord -D ${DEVICE} -f ${QUALITY} -t wav -c ${CHANNELS} -r ${SAMPLE_RATE} -d ${CHUNK_SIZE_IN_S} --use-strftime ${file}.wav
+    bash ${LOCATION} ${file}.wav ${ts} ${CHANNELS} &
+done
